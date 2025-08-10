46bae31cdd9a0eb111e04ab129971c018bf29747
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-10 22:49:36 +0800

    Multiplier Calculator

--- CCalculator.c
+++ CCalculator.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main(){
-    int results = 0;
-    float num1 = '\0';
-    float num2 = '\0';
+    double results = 0.0;
+    double num1 = 0.0;
+    double num2 = 0.0;
     char user[50] = "";
     
     printf("Type your username: ");
@@ -12,21 +13,20 @@ int main(){
     user[strlen(user) - 1] = '\0';
     
     printf("\n\nHello, %s \n", user);
-    printf("I will give you a brief description of this code, you can only add you can't subtract, divide, and multiply\n");
+    printf("I will give you a brief description of this code, you can only multiply you can't add, subtract, divide\n");
     printf("\nPress enter to continue \n");
     
     getchar();
-    printf("Addition Calculator\n");
+    printf("Multiplier Calculator\n");
     printf("--------------------------\n");
     printf("Type your first number: ");
-    scanf("%f", &num1);
+    scanf("%lf", &num1);
 
     printf("Type your second number: ");
-    scanf("%f", &num2);
-
-    results = num1 + num2;
+    scanf("%lf", &num2);
+    results = num1 * num2;
     
-    printf("Result: %d", results);
+    printf("Result: %.2lf", results);
     
     return 0;
 }
