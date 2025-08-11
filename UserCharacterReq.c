127e9a9975cc4ec7ad501ebfbc9e9e8d4f225390
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-11 20:50:18 +0800

    Basic Character Requirement for User

---
+++ UserCharacterReq.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main() {
+    char name[50] = "";
+    
+
+    printf("Must have 10+ more characters\n");
+    printf("Enter your username: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+    
+    if(strlen(name) >= 11){
+        printf("Hello %s", name);
+    }
+    else if(strlen(name) == 10){
+        printf("Hello %s", name);
+    }
+    else{
+        printf("You have entered the insufficient characters in your user, please try again.");
+    }
+    return 0;
+}
