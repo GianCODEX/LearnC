6bd3d7bf21ca9e7d5bcfb4f43cf065b28212f65b
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-12 21:38:10 +0800

    Switch Function
    Removed if-else statement and changed it to switch statement

--- WeightConverter.c
+++ WeightConverter.c
@@ -8,21 +8,21 @@ int main(){
     printf("Weight Conversion Calculator\n");
     printf("1. Kilograms to Pounds\n");
     printf("2. Pounds to Kilograms\n");
-    printf("Enter your choice: \n\n");
+    printf("\nEnter your choice: ");
     scanf("%d", &choice);
-    
-    if(choice == 1){
-        printf("Enter the weight in Kilograms: ");
-        scanf("%lf", &kilograms);
-        printf("%.2f kilograms is equal to %.2f pounds\n", kilograms, kilograms * 2.20462);
-    }
-    else if(choice == 2){
-        printf("Enter the weight in pounds: ");
-        scanf("%lf", &pounds);
-        printf("%.2lf pounds is equal to %.2lf kilograms\n", pounds, pounds / 2.20462);
-    }
-    else{
-        printf("Invalid Choice! Please enter 1 or 2\n");
+    switch(choice){
+        case 1:
+            printf("Enter the weight in kilograms: ");
+            scanf("%lf", &kilograms);
+            printf("%.2f kilograms is equal to %.2f pounds\n", kilograms, kilograms * 2.204620);
+        break;
+        case 2:
+            printf("Enter the weight in pounds: ");
+            scanf("%lf", pounds);
+            printf("%.2f pounds is equal to %.2f kilograms\n", pounds, pounds / 2.204620);
+        break;
+        default:
+            printf("Try again! Pick 1 or 2");   
     }
     return 0;
 }
