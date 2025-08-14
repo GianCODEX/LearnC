4299a0f598adb288143d91cda31d3978efcaf497
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-14 22:12:14 +0800

    Operators and switches
    Removed user information, added division + addition + subtraction + multiplication

--- CCalculator.c
+++ CCalculator.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 
 int main(){
-    double results = 0.0;
-    double num1 = 0.0;
-    double num2 = 0.0;
-    int age = 0;
-    char user[50] = "";
     
-    printf("Type your username: ");
-    fgets(user, sizeof(user), stdin);
-    user[strlen(user) - 1] = '\0';
+    char operator = '\0';
+    double num1 = 0.0f;
+    double num2 = 0.0f;
+    double result = 0.0f;
     
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
     
-    if(age >= 18){
-         printf("\n\nHello, %s \n", user);
-         printf("I will give you a brief description of this code, you can only multiply you can't add, subtract, divide\n");
-         printf("\nPress enter to continue \n");
+    printf(" ( +  -  * / ) \n");
+    printf("Choose an operator: ");
+    scanf(" %c", &operator);
     
-         getchar();
-         printf("Multiplier Calculator\n");
-         printf("--------------------------\n");
-         printf("Type your first number: ");
-         scanf("%lf", &num1);
-
-         printf("Type your second number: ");
-        scanf("%lf", &num2);
-         results = num1 * num2;
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
     
-         printf("Result: %.2lf", results);
-    }
-    else{
-        printf("You are not old enough to use this... ");
+    switch(operator){
+        case '+':
+        result = num1 + num2;
+            printf("The total add is... %.2lf", result);
+            break;
+        case '-':
+        result = num1 - num2;
+            printf("The total sub is... %.2lf", result);
+            break;
+        case '*':
+        result = num1 * num2;        
+            printf("The total multiply is... %.2lf", result);
+            break;
+        case '/':
+        if(num2 == 0){
+            printf("You can't divide by zero!");
+        }
+        else{
+            result = num1 / num2;      
+            printf("The total divide is... %.2lf", result);
+        }
+            break;
+            default:
+                printf("Invalid operator...\n");
     }
     return 0;
 }
