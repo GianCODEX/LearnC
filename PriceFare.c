4b7423e85e09c8a321c98aab98b864e067bc236b
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-14 23:06:30 +0800

    LipaPriceFare
    added nested if else statement + Destination (MataasNaKahoy/MataasNaLupa)

--- PriceFare.c
+++ PriceFare.c
@@ -3,10 +3,15 @@
 #include <string.h>
 
 float jeepPrice = 15.00;
+float jeepPriceToKahoy = 25.00;
 bool is_Student = false;
 bool is_Senior = false;
+bool is_MataasNaKahoy = false;
+bool is_MataasNaLupa = false;
 
+char mataasNaKahoy[50];
 char name[50];
+char destination[50];
 char isStatus[10];
 
 int main() {
@@ -14,6 +19,21 @@ int main() {
     printf("Enter your name: ");
     fgets(name, sizeof(name), stdin);
     name[strcspn(name, "\n")] = '\0';  // remove newline
+
+    // Get destination
+    printf("Choose your Destination \n(Mataas na Kahoy / Mataas na Lupa): ");
+    fgets(destination, sizeof(destination), stdin);
+    destination[strcspn(destination, "\n")] = '\0'; // remove newline
+
+    // Identify location
+    if (strcasecmp(destination, "Mataas na Kahoy") == 0) {
+        is_MataasNaKahoy = true;
+    } else if (strcasecmp(destination, "Mataas na Lupa") == 0) {
+        is_MataasNaLupa = true;
+    } else {
+        printf("Invalid destination. Exiting.\n");
+        return 1;
+    }
 
     // Check student status
     printf("Are you a student? (yes/no): ");
@@ -32,25 +52,36 @@ int main() {
     }
 
     // Apply discounts
-    if (is_Student) {
-        if(is_Senior){
-            printf("You have a discount of 40%\n");
-            jeepPrice *= 0.6;
+    if (is_MataasNaKahoy) {
+        if (is_Student && is_Senior) {
+            printf("You have a discount of 40%%\n");
+            jeepPriceToKahoy *= 0.6;
+        } else if (is_Student) {
+            printf("You have a student discount of 20%%\n");
+            jeepPriceToKahoy *= 0.8;
+        } else if (is_Senior) {
+            printf("You have a senior discount of 20%%\n");
+            jeepPriceToKahoy *= 0.8;
+        } else {
+            printf("\nHello %s, the fare price to %s is: %.2f\n", name, destination, jeepPriceToKahoy);
+            return 0;
         }
-        else{
-            printf("You have a student discount of 20%\n");
-            jeepPrice *= 0.8;
-        }
-    }
-    else {
-        if(is_Senior){
-            printf("You have a senior discount of 20%\n");
-            jeepPrice *= 0.8;
-        }
+        printf("\nHello %s, the fare price to %s is: %.2f\n", name, destination, jeepPriceToKahoy);
     }
 
-    // Output result
-    printf("\nHello %s, the fare price is: %.2f\n", name, jeepPrice);
+    if (is_MataasNaLupa) {
+        if (is_Student && is_Senior) {
+            printf("You have a discount of 40%%\n");
+            jeepPrice *= 0.6;
+        } else if (is_Student) {
+            printf("You have a student discount of 20%%\n");
+            jeepPrice *= 0.8;
+        } else if (is_Senior) {
+            printf("You have a senior discount of 20%%\n");
+            jeepPrice *= 0.8;
+        }
+        printf("\nHello %s, the fare price to %s is: %.2f\n", name, destination, jeepPrice);
+    }
 
     return 0;
 }
