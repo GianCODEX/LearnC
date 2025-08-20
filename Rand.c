fd1bf469ea007c273bd757a647952cb6df1bd9d3
GianCODEX <gianrossbcorpuz@gmail.com>
2025-08-21 00:24:26 +0800

    INSERTED GUESS NUMBER GAME

--- Rand.c
+++ Rand.c
@@ -25,3 +25,43 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+    
+    srand(time(NULL));
+    
+    int guess = 0;
+    int tries = 0;
+    int min = 1;
+    int max = 100;
+    int answer = (rand() & (max - min + 1)) + min;
+    
+    printf("*** GUESS THE NUMBER ***\n");
+    
+    do{
+        printf("Guess a number between %d - %d: ",min, max );
+        scanf("%d", &guess);
+        tries++;
+        
+        if(guess < answer){
+            printf("TOO LOW!\n");
+        } 
+        else if (guess > answer){
+            printf("TOO HIGH!\n");
+        } 
+        else {
+            printf("YOU'RE CORRECT!\n");
+        }
+        
+    }while(guess != answer);
+    
+    printf("The answer is %d\n", answer);
+    printf("It took you %d tries\n", tries);
+    
+    return 0;
+    
+}
